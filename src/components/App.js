8cd015c15d5c1055371b02c478571594ab7a4fe3
OiR-ptr <myr.ryusuke@gmail.com>
2020-07-02 16:26:50 +0900

    :wrench: use prettier

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+function App() {
+  return (
+    <AppBar>
+      <Toolbar></Toolbar>
+    </AppBar>
+  );
+}
+
+export default App;
---
+++ src/reducers/GameReducer.jsx
@@ -0,0 +1,9 @@
+const initialState = {};
+
+export default function gameReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      break;
+  }
+  return state;
+}
